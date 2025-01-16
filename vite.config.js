661af8718d25f02c58d9896c927e887e4d07bd54
vera916developer <estebantorovera1@gmail.com>
2025-01-16 16:00:17 -0500

    fix: init vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    test: {
+    environment:"jsdom",
+    globals: true,
+    include: ["test/**"],
+    coverage:{
+      exclude:[
+        'src/main.jsx',
+        'eslint.config.js',
+        'vite.config.js',
+        'dist'
+      ]  
+   }
+  }
 })
